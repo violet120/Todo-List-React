62100f7acf9c9a74e22a007335ec06a64f74c0cb
violet120 <ai1397de@gmail.com>
2023-03-06 11:31:30 +0800

    add db.json and dummy todos

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,29 +1,6 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
 import { useState } from 'react';
 
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
-
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('')
   const [todos, setTodos] = useState(dummyTodos)
