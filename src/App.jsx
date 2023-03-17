3444856112aeb7f442e7281cd47e07d82d7caba8
violet120 <ai1397de@gmail.com>
2023-03-17 17:53:00 +0800

    router basename

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages';
 import { AuthProvider } from 'contexts/AuthContext';
 
+const basename = process.env.PUBLIC_URL
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <AuthProvider>
           <Routes>
             <Route path="login" element={<LoginPage />} />
